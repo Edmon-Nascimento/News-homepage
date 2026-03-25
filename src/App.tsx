976794d6d97e623f2a249603619315dd88ffcf0e
EDMON NASCIMENTO SANTOS <edmoncode7@gmail.com>
2026-03-25 10:59:10 -0300

    feat: mobile version

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,98 @@
-import './App.css'
-import Header from './components/Header'
-import retroPcImg from './assets/image-retro-pcs.jpg'
-import laptopImg from './assets/image-top-laptops.jpg'
-import gameImg from './assets/image-gaming-growth.jpg'
+import Header from "./components/Header";
+import retroPcImg from "./assets/image-retro-pcs.jpg";
+import laptopImg from "./assets/image-top-laptops.jpg";
+import gameImg from "./assets/image-gaming-growth.jpg";
+import desktopImg from "./assets/image-web-3-desktop.jpg";
+import mobileImg from "./assets/image-web-3-mobile.jpg";
 
 function App() {
   return (
     <>
-      <Header/>
-      <main>
-        
-        <section className='hero'>
-          <div className='banner'></div>
-          <h1>The Bright Future of Web 3.0?</h1>
-          <p>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?</p>
-          <button>Read more</button>
+      <Header />
+      <main className="p-4">
+        <picture>
+          <source media="(min-width: 996px)" srcSet={desktopImg} />
+          <img src={mobileImg} alt="" className="w-full" />
+        </picture>
+
+        <section className="">
+          <h1 className="mt-8 text-5xl font-bold text-[#00001A]">The Bright Future of Web 3.0?</h1>
+          <p className="py-4 text-[#868195]">
+            We dive into the next evolution of the web that claims to put the
+            power of the platforms back into the hands of the people. But is it
+            really fulfilling its promise?
+          </p>
+          <button className="bg-[#F05E50] px-4 py-2 font-bold cursor-pointer mb-12">
+            Read more
+          </button>
         </section>
 
-        <section className='new'>
-          <h2>New</h2>
+        <section className="py-4 px-6 bg-[#00001A] mb-20">
+          <h2 className="text-[#E8AF53] text-4xl font-bold mb-5">New</h2>
 
-          <div className="new-topic">
-            <h3>Hydrogen VS Electric Cars</h3>
-            <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
-          </div>
-          
-          <div className="divisor"></div>
-
-          <div className="new-topic">
-            <h3>The Downsides of AI Artistry</h3>
-            <p>What are the possible adverse effects of on-demand AI image generation?</p>
+          <div>
+            <h3 className="text-white font-bold text-xl pt-6 pb-3">Hydrogen VS Electric Cars</h3>
+            <p className="text-white/70 pb-6">Will hydrogen-fueled cars ever catch up to EVs?</p>
           </div>
 
-          <div className="divisor"></div>
+          <div className="border border-amber-50/30 w-full"></div>
 
-          <div className="new-topic">
-            <h3>Is VC Funding Drying Up?</h3>
-            <p>Private funding by VC firms is down 50% YOY. We take a look at what that means.</p>
+          <div>
+            <h3 className="text-white font-bold text-xl pt-6 pb-3">The Downsides of AI Artistry</h3>
+            <p className="text-white/70 pb-6">
+              What are the possible adverse effects of on-demand AI image
+              generation?
+            </p>
+          </div>
+
+          <div className="border border-amber-50/30 w-full"></div>
+
+          <div>
+            <h3 className="text-white font-bold text-xl pt-6 pb-3">Is VC Funding Drying Up?</h3>
+            <p className="text-white/70 pb-6">
+              Private funding by VC firms is down 50% YOY. We take a look at
+              what that means.
+            </p>
           </div>
         </section>
 
-        <section className='popular'>
-          <div className="popular-topic">
-            <div className="popular-topic-bg">
-              <img src={retroPcImg} alt="" />
+        <section className="flex flex-col gap-10">
+          <div className="grid grid-rows-1 grid-cols-[auto_1fr] gap-5">
+            <div className="w-32">
+              <img className="" src={retroPcImg} alt="" />
             </div>
-            <div className='popular-topic-text'>
-              <h1>01</h1>
-              <h2>Reviving Retro PCs</h2>
-              <p>What happens when old PCs are given modern upgrades? </p>
-            </div>
-          </div>
-
-          <div className="popular-topic">
-            <div className="popular-topic-bg">
-              <img src={laptopImg} alt="" />
-            </div>
-            <div className='popular-topic-text'>
-              <h1>02</h1>
-              <h2>Top 10 Laptops of 2022</h2>
-              <p>Our best picks for various needs and budgets. </p>
+            <div className="flex flex-col gap-3">
+              <h1 className="text-3xl font-bold text-[#F15D51]">01</h1>
+              <h2 className="font-bold text-[#00001A]">Reviving Retro PCs</h2>
+              <p className="text-[#868195] ">What happens when old PCs are given modern upgrades?</p>
             </div>
           </div>
 
-          <div className="popular-topic">
-            <div className="popular-topic-bg">
-              <img src={gameImg} alt="" />
+          <div className="grid grid-rows-1 grid-cols-[auto_1fr] gap-5">
+            <div className="w-32">
+              <img className="" src={laptopImg} alt="" />
             </div>
-            <div className='popular-topic-text'>
-              <h1>03</h1>
-              <h2>The Growth of Gaming</h2>
-              <p>How the pandemic has sparked fresh opportunities.</p>
+            <div className="flex flex-col gap-3">
+              <h1 className="text-3xl font-bold text-[#F15D51]">02</h1>
+              <h2 className="font-bold text-[#00001A]">Top 10 Laptops of 2022</h2>
+              <p className="text-[#868195] ">Our best picks for various needs and budgets. </p>
+            </div>
+          </div>
+
+          <div className="grid grid-rows-1 grid-cols-[auto_1fr] gap-5">
+            <div className="w-32">
+              <img className="" src={gameImg} alt="" />
+            </div>
+            <div className="flex flex-col gap-3">
+              <h1 className="text-3xl font-bold text-[#F15D51]">03</h1>
+              <h2 className="font-bold text-[#00001A]">The Growth of Gaming</h2>
+              <p className="text-[#868195] ">How the pandemic has sparked fresh opportunities.</p>
             </div>
           </div>
         </section>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,57 +1,49 @@
-import logoImg from '../../assets/logo.svg'
-import openMenuIcon from '../../assets/icon-menu.svg'
-import closeMenuIcon from '../../assets/icon-menu-close.svg'
-import { useState } from "react"
+import logoImg from "../../assets/logo.svg";
+import openMenuIcon from "../../assets/icon-menu.svg";
+import closeMenuIcon from "../../assets/icon-menu-close.svg";
+import { useState } from "react";
 
+function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-function Header(){
+  function handleMenu() {
+    setIsMenuOpen((prev) => !prev);
+  }
 
-    const [isMenuOpen, setIsMenuOpen] = useState(false)
+  return (
+    <>
+      <header className="flex items-center justify-between p-4">
+        <img className="" src={logoImg} alt="newspapper-logo" />
+        <button
+          className="z-99"
+          onClick={() => {
+            handleMenu();
+          }}
+        >
+          <img
+            src={`${isMenuOpen ? closeMenuIcon : openMenuIcon}`}
+            alt=""
+            className="lg:hidden"
+          />
+        </button>
+        <nav
+          className={`${isMenuOpen ? "flex" : "hidden"} z-99 fixed top-0 left-0 h-screen w-50 bg-black/80 text-white flex-col items-end p-4 gap-3 lg:flex lg:flex-row lg:bg-transparent lg:static lg:w-auto lg:h-auto lg:text-black
+          
+          `}
+        >
+          <a href="#">Home</a>
+          <a href="#">New</a>
+          <a href="#">Popular</a>
+          <a href="#">Trending</a>
+          <a href="#">Categories</a> 
+        </nav>
+      </header>
 
-    function handleMenu(){
-        setIsMenuOpen( prev => !prev)
-    }
-
-    return(
-        <>
-            <header>
-                <img className="logo" src={logoImg} alt="newspapper-logo" />
-                <button className="btn-menu" onClick={()=>{handleMenu()}}>
-                    <img src={openMenuIcon} alt=""/>
-                </button>
-
-                <nav className="menu-desktop">
-                    <a href="#">Home</a>
-                    <a href="#">New</a>
-                    <a href="#">Popular</a>
-                    <a href="#">Trending</a>
-                    <a href="#">Categories</a>
-                </nav>
-                
-            </header>
-
-            {isMenuOpen && (
-                <div className="overlay active" onClick={handleMenu}></div>
-            )}  
-
-
-
-            <aside className= {isMenuOpen ? "active" : "inactive"}>
-                <div className="btn-container">
-                    <button className="btn-menu" onClick={()=>{handleMenu()}}>
-                    <img src={closeMenuIcon} alt=""/>
-                </button>
-                </div>
-                <nav className="menu-mobile">
-                    <a href="#" onClick={()=>{handleMenu()}}>Home</a>
-                    <a href="#" onClick={()=>{handleMenu()}}>New</a>
-                    <a href="#" onClick={()=>{handleMenu()}}>Popular</a>
-                    <a href="#" onClick={()=>{handleMenu()}}>Trending</a>
-                    <a href="#" onClick={()=>{handleMenu()}}>Categories</a>
-                </nav>
-            </aside>
-        </>
-    )
+      {isMenuOpen && (
+        <div className="w-screen h-screen bg-black/50 fixed top-0 left-0" onClick={handleMenu}></div>
+      )}
+    </>
+  );
 }
 
-export default Header
+export default Header;
